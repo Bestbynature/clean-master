9b44abaddbeb856d3f654fc6009ee1813192274c
Damilare <bestbynature2010@gmail.com>
2024-10-23 11:32:45 +0100

    Update validations for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-  # Associations
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  
+         # Associations
   has_many :jobs
   has_many :enquiries
   has_many :invoices
@@ -11,4 +20,6 @@ class User < ApplicationRecord
   # Validations
   validates :email, presence: true, uniqueness: true
   validates :role, presence: true # 'client' or 'vendor'
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
